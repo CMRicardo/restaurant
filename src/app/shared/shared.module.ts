88e47d7402dd2babba2e792af0fd82801a5cd0da
Ricardo Corrales <ricardocorrales84@gmail.com>
2023-10-24 19:35:44 -0600

    fix: :fire: Removí el layout del shared y el componente container
    Porque ya no se necesitan

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,26 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LayoutComponent } from './layout/layout.component';
+
 import { HeaderComponent } from './components/header/header.component';
-import { ContainerComponent } from './components/container/container.component';
 import { SearchbarComponent } from './components/searchbar/searchbar.component';
 import { WelcomePageComponent } from './pages/welcome-page/welcome-page.component';
 
-
-
 @NgModule({
   declarations: [
-    LayoutComponent,
     HeaderComponent,
-    ContainerComponent,
     SearchbarComponent,
     WelcomePageComponent
   ],
   imports: [
     CommonModule
   ],
-  exports: [
-    LayoutComponent,
-  ]
 })
 export class SharedModule { }
