917c6cac56ac5f7d5d6624d04b21e06fc0c8aaa0
Ricardo Corrales <ricardocorrales84@gmail.com>
2023-10-25 17:57:31 -0600

    feat: :sparkles: Hice que el boton de login nos lleve al inicio

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   templateUrl: './login-page.component.html',
 })
 export class LoginPageComponent {
+  private router = inject(Router)
 
+  login(e: MouseEvent): void {
+    e.preventDefault()
+    console.log('Hola mundo!');
+    this.router.navigateByUrl('app/home')
+  }
 }
