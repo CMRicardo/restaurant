d2b0620223d2b9ae3dec52ad897f10a7991afcb6
Ricardo Corrales <ricardocorrales84@gmail.com>
2023-10-25 18:32:36 -0600

    feat: :sparkles: Estoy intentando cambiar el nombre de la barra de navegación

--- src/app/restaurant/layouts/restaurant-layout/restaurant-layout.component.ts
+++ src/app/restaurant/layouts/restaurant-layout/restaurant-layout.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   templateUrl: './restaurant-layout.component.html',
 })
 export class RestaurantLayoutComponent {
+  
+  private readonly titleService = inject(Title)
+  public title: string = ''
 
+  ngOnInit(): void {
+    const fullTitle = this.titleService.getTitle()
+    this.title = fullTitle.split(' ')[0]
+    
+  }
 }
