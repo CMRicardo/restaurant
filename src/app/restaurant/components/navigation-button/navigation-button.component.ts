bad8c5f7e0d06967f03fbcbbf0ab390a4bb704af
Ricardo Corrales <ricardocorrales84@gmail.com>
2023-10-27 14:51:05 -0600

    fix: :bug: Ahora los iconos de los botones son del color correcto
    Y el layout de los botones ya no está roto

--- src/app/restaurant/components/navigation-button/navigation-button.component.ts
+++ src/app/restaurant/components/navigation-button/navigation-button.component.ts
@@ -3,12 +3,9 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-navigation-button',
   templateUrl: './navigation-button.component.html',
-  styles: [
-  ]
 })
 export class NavigationButtonComponent {
   @Input() url: string = ''
   @Input() title: string = ''
-  @Input() imageUrl: string = ''
   @Input() color: string = ''
 }
