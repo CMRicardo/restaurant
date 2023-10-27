f63bbe7bee419672d4375dbf569d762691fe4fca
Ricardo Corrales <ricardocorrales84@gmail.com>
2023-10-27 14:18:54 -0600

    refactor: :fire: Removí los archivos de pruebas y estilos del welcome page

--- src/app/shared/pages/welcome-page/welcome-page.component.ts
+++ src/app/shared/pages/welcome-page/welcome-page.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   templateUrl: './welcome-page.component.html',
-  styleUrls: ['./welcome-page.component.css']
 })
 export class WelcomePageComponent {
 
