33a93ed29c1ff848b3df9316d2627261bc34848e
Ricardo Corrales <ricardocorrales84@gmail.com>
2023-10-27 18:15:02 -0600

    feat: :sparkles: Ahora la página inicial es la "Welcome"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { WelcomePageComponent } from './shared/pages/welcome-page/welcome-page.component';
 
 const routes: Routes = [
-  { path: 'welcome', component: WelcomePageComponent },
+  { path: '', component: WelcomePageComponent },
   {
     path: 'auth',
     loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule),
