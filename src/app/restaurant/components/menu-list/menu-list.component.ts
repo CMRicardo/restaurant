7c5fa0df6206ec144053488cd3d18db0b88aa8e3
MrLowell <98554705+MrLowell@users.noreply.github.com>
2023-10-29 11:23:34 -0600

    Agregue un contenedor de tagetas

---
+++ src/app/restaurant/components/menu-list/menu-list.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './menu-list.component.html',
+  selector: "app-menu-list",
+  styles: [
+  ]
+})
+export class MenuListComponent {
+
+  items = [
+    'Elemento 1',
+    'Elemento 2',
+    'Elemento 3',
+    'Elemento 4',
+    'Elemento 4',
+    'Elemento 4',
+    'Elemento 4',
+    'Elemento 4',
+    'Elemento 4',
+    'Elemento 4',
+    'Elemento 4',
+    'Elemento 4',
+
+
+  ];
+
+}
