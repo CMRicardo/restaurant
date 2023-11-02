03f259c29d1b8e2ac37aeacd708e334840f6e536
Ricardo Corrales <ricardocorrales84@gmail.com>
2023-11-02 17:37:59 -0600

    feat: :sparkles: Agregé algunos elementos de la pantalla new-order

--- src/app/restaurant/restaurant.module.ts
+++ src/app/restaurant/restaurant.module.ts
@@ -3,61 +3,65 @@ import { CommonModule } from '@angular/common';
 
 import { RestaurantRoutingModule } from './restaurant-routing.module';
 import { RestaurantLayoutComponent } from './layouts/restaurant-layout/restaurant-layout.component';
-import { HomePageComponent } from './pages/home-page/home-page.component';
+
 import { SharedModule } from '../shared/shared.module';
-import { NewOrderPageComponent } from './pages/new-order-page/new-order-page.component';
-import { MenuPageComponent } from './pages/menu-page/menu-page.component';
-import { OrdersPageComponent } from './pages/orders-page/orders-page.component';
-import { ComplaintsPageComponent } from './pages/complaints-page/complaints-page.component';
-import { PromotionsPageComponent } from './pages/promotions-page/promotions-page.component';
-import { RecordsPageComponent } from './pages/records-page/records-page.component';
-import { NavigationButtonComponent } from './components/navigation-button/navigation-button.component';
-import { PromotionsIconComponent } from './components/icons/promotions-icon/promotions-icon.component';
-import { NewOrderIconComponent } from './components/icons/new-order-icon/new-order-icon.component';
-import { MenuIconComponent } from './components/icons/menu-icon/menu-icon.component';
-import { DueOrdersIconComponent } from './components/icons/due-orders-icon/due-orders-icon.component';
+
 import { ComplaintsIconComponent } from './components/icons/complaints-icon/complaints-icon.component';
-import { RecordsIconComponent } from './components/icons/records-icon/records-icon.component';
+import { ComplaintsPageComponent } from './pages/complaints-page/complaints-page.component';
+import { DueOrdersIconComponent } from './components/icons/due-orders-icon/due-orders-icon.component';
+import { EmployeeIconComponent } from './components/icons/employee-icon/employee-icon.component';
+import { EmployeesPageComponent } from './pages/employees-page/employees-page.component';
+import { FilterOptionsComponent } from './components/filter-options/filter-options.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { MenuIconComponent } from './components/icons/menu-icon/menu-icon.component';
 import { MenuItemComponent } from './components/menu-item/menu-item.component';
 import { MenuListComponent } from './components/menu-list/menu-list.component';
-import { EmployeesPageComponent } from './pages/employees-page/employees-page.component';
-import { SupplierPageComponent } from './pages/supplier-page/supplier-page.component';
-import { SalesPageComponent } from './pages/sales-page/sales-page.component';
-import { ShoppingPageComponent } from './pages/shopping-page/shopping-page.component';
-import { EmployeeIconComponent } from './components/icons/employee-icon/employee-icon.component';
+import { MenuPageComponent } from './pages/menu-page/menu-page.component';
+import { NavigationButtonComponent } from './components/navigation-button/navigation-button.component';
+import { NewOrderIconComponent } from './components/icons/new-order-icon/new-order-icon.component';
+import { NewOrderPageComponent } from './pages/new-order-page/new-order-page.component';
+import { OrdersPageComponent } from './pages/orders-page/orders-page.component';
+import { PromotionsIconComponent } from './components/icons/promotions-icon/promotions-icon.component';
+import { PromotionsPageComponent } from './pages/promotions-page/promotions-page.component';
+import { RecordsIconComponent } from './components/icons/records-icon/records-icon.component';
+import { RecordsPageComponent } from './pages/records-page/records-page.component';
 import { SalesIconComponent } from './components/icons/sales-icon/sales-icon.component';
-import { SupplierIconComponent } from './components/icons/supplier-icon/supplier-icon.component';
+import { SalesPageComponent } from './pages/sales-page/sales-page.component';
+import { SearchbarComponent } from './components/searchbar/searchbar.component';
 import { ShoppingIconComponent } from './components/icons/shopping-icon/shopping-icon.component';
-import { FilterOptionsComponent } from './components/filter-options/filter-options.component';
+import { ShoppingPageComponent } from './pages/shopping-page/shopping-page.component';
+import { SupplierIconComponent } from './components/icons/supplier-icon/supplier-icon.component';
+import { SupplierPageComponent } from './pages/supplier-page/supplier-page.component';
 
 @NgModule({
   declarations: [
-    RestaurantLayoutComponent,
-    HomePageComponent,
-    NewOrderPageComponent,
-    MenuPageComponent,
-    OrdersPageComponent,
-    ComplaintsPageComponent,
-    PromotionsPageComponent,
-    RecordsPageComponent,
-    NavigationButtonComponent,
-    PromotionsIconComponent,
-    NewOrderIconComponent,
-    MenuIconComponent,
-    DueOrdersIconComponent,
     ComplaintsIconComponent,
-    RecordsIconComponent,
+    ComplaintsPageComponent,
+    DueOrdersIconComponent,
+    EmployeeIconComponent,
+    EmployeesPageComponent,
+    FilterOptionsComponent,
+    HomePageComponent,
+    MenuIconComponent,
     MenuItemComponent,
     MenuListComponent,
-    EmployeesPageComponent,
-    SupplierPageComponent,
-    SalesPageComponent,
-    ShoppingPageComponent,
-    EmployeeIconComponent,
+    MenuPageComponent,
+    NavigationButtonComponent,
+    NewOrderIconComponent,
+    NewOrderPageComponent,
+    OrdersPageComponent,
+    PromotionsIconComponent,
+    PromotionsPageComponent,
+    RecordsIconComponent,
+    RecordsPageComponent,
+    RestaurantLayoutComponent,
     SalesIconComponent,
-    SupplierIconComponent,
+    SalesPageComponent,
+    SearchbarComponent,
     ShoppingIconComponent,
-    FilterOptionsComponent,
+    ShoppingPageComponent,
+    SupplierIconComponent,
+    SupplierPageComponent,
   ],
   imports: [
     CommonModule,
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HeaderComponent } from './components/header/header.component';
-import { SearchbarComponent } from './components/searchbar/searchbar.component';
 import { WelcomePageComponent } from './pages/welcome-page/welcome-page.component';
 import { RouterModule } from '@angular/router';
 import { NavLinkComponent } from './components/nav-link/nav-link.component';
@@ -10,7 +9,6 @@ import { NavLinkComponent } from './components/nav-link/nav-link.component';
 @NgModule({
   declarations: [
     HeaderComponent,
-    SearchbarComponent,
     WelcomePageComponent,
     NavLinkComponent,
   ],
