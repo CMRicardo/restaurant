c1986ad1f0d8a2bb5b5111d04d96068fffb5f176
emilssonsoler <edennirs@gmail.com>
2023-11-18 15:17:44 -0600

    selector imagen con su previsualizacion

--- src/app/restaurant/components/formulario-platillo/formulario-platillo.component.ts
+++ src/app/restaurant/components/formulario-platillo/formulario-platillo.component.ts
@@ -8,6 +8,30 @@ import { Component } from '@angular/core';
 })
 export class FormularioPlatilloComponent {
 
+  imageUrl: string | ArrayBuffer | null = null;
+
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0];
+    this.previewImage(file);
+  }
+
+  previewImage(file: File) {
+    const reader = new FileReader();
+
+    reader.readAsDataURL(file);
+
+    reader.onload = () => {
+      this.imageUrl = reader.result;
+      this.uploadImage()
+    };
+
+  }
+
+  uploadImage() {
+    // Aquí puedes agregar lógica para subir la imagen al servidor si es necesario
+    if(this.imageUrl)
+      console.log(this.imageUrl.toString().split(',')[1]);
+  }
 onSubmit(event:Event) {
 event.preventDefault()
 throw new Error('Method not implemented.');
