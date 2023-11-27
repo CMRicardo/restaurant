0da0727a9b6fe78baafab8ed1e4cca6b8f98cf79
Ricardo Corrales <ricardocorrales84@gmail.com>
2023-11-27 09:36:01 -0600

    feat: :sparkles: Ahora funciona el login

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,14 +1,20 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   templateUrl: './login-page.component.html',
 })
-export class LoginPageComponent {
+export class LoginPageComponent implements OnInit {
   private router = inject(Router)
+  private authService = inject(AuthService)
   private formBuilder = inject(FormBuilder)
-
+  
+  async ngOnInit(): Promise<void> {
+    await this.authService.getUsers()
+  }
+  
   public myForm = this.formBuilder.group({
     email: ['', [Validators.required, Validators.email]],
     password: ['', [Validators.required, Validators.minLength(6)]]
@@ -29,10 +35,17 @@ export class LoginPageComponent {
   }
 
   public login(): void {
-    if (this.myForm.invalid) {
+    if (this.myForm.invalid) return
+    const email = String(this.email?.value)
+    const password = String(this.password?.value)
+    if (!email && !password) return
+
+    this.authService.currentUser = this.authService.validatedCredentials({ email, password })
+    if (this.authService.currentUser){
+      this.router.navigateByUrl('app/home')
       return
     }
-
-    this.router.navigateByUrl('app/home')
+    // Cambiar por un error
+    alert('Credenciales no validas')
   }
 }
---
+++ src/app/auth/services/auth.service.ts
@@ -0,0 +1,27 @@
+import { Injectable, OnInit } from '@angular/core';
+import { Employee, validatedCredentialsProps } from '../interfaces/customers-response.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private API_URL = 'https://litoral-restaurant-api.1.us-1.fl0.io/employees'
+  public employees: Employee[] = []
+  public currentUser?: Employee
+
+  public validatedCredentials({ email, password }: validatedCredentialsProps) {
+    const employeeIndex = this.employees.findIndex(employee => {
+      return employee.password === password && employee.email === email
+    })
+    console.log(this.employees[employeeIndex]);
+    if (employeeIndex === -1) return
+    
+    return this.employees[employeeIndex]
+  }
+
+  public async getUsers() {
+    const res = await fetch(this.API_URL)
+    const data = await res.json()
+    this.employees = data
+  }
+}
