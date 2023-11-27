7a09a72bec3e6893f2658a7e2cb37498540c597b
Ricardo Corrales <ricardocorrales84@gmail.com>
2023-11-27 09:46:10 -0600

    feat: :sparkles: Ahora el user-info-component toma los datos del currentUser

--- src/app/shared/components/user-info/user-info.component.ts
+++ src/app/shared/components/user-info/user-info.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { Employee } from 'src/app/auth/interfaces/customers-response.interface';
+import { AuthService } from 'src/app/auth/services/auth.service';
+
+const DEFAULT_USER = { "id": "72101198-81c3-11ee-b53e-eaf15ba8dff1", "employeeType": "Gerente", "firstName": "Daniel", "secondName": "Alejandro", "lastName": "Corrales", "idNumber": "0511201103483", "phoneNumber": "12345678", "email": "daniel@litoral.com", "password": "admin123", "profilePictureUrl": "https://randomuser.me/api/portraits/men/80.jpg", "address": "Villanueva" }
 
 @Component({
   selector: 'app-user-info',
@@ -6,10 +10,15 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class UserInfoComponent {
-  public user = {
-    profilePic: 'https://randomuser.me/api/portraits/men/35.jpg',
-    name: 'Jhon Doe',
-    email: 'hello@email.com'
+export class UserInfoComponent implements OnInit {
+  private authService = inject(AuthService)
+  public user?: Employee
+
+  ngOnInit(): void {
+    this.user = this.authService!.currentUser
+    if (!this.user) {
+      this.user = DEFAULT_USER
+    }
   }
+
 }
